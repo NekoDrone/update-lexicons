802fd37b15b21aa47bd8b1b1e994395f81af7146
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-02-12 18:09:59 +0800

    feat: add tsdown for bundling

--- shell.nix
+++ shell.nix
@@ -9,6 +9,7 @@
   pnpm,
   typescript,
   typescript-language-server,
+  act,
 
   callPackage,
 }:
@@ -25,6 +26,7 @@ mkShellNoCC {
     pnpm
     typescript
     typescript-language-server
+    act
   ];
 
   shellHook = ''
