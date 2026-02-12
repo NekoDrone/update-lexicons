a64c1fd8eadf632d520cb75f33d42b9b746b816b
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-02-12 19:38:09 +0800

    feat: more test cases

--- src/read-lexicons.ts
+++ src/read-lexicons.ts
@@ -28,33 +28,60 @@ export const readLexicons = async (workspaceRoot: string) => {
         const content: unknown = JSON.parse(raw);
 
         // basic sanity checks
-        if (typeof content !== "object" || !content) continue;
+        if (typeof content !== "object" || !content) {
+            console.warn(
+                "Parsed .json file was not a properly formatted json object. Check your inputs.",
+            );
+            continue;
+        }
 
         // check for id matching location of file
-        if (!("id" in content)) continue;
+        if (!("id" in content)){
+            console.warn(
+                "Skipping lexicon with no `id` field. Found at",
+                `\`${relativePath}\`.`,
+                "Per the spec, you need to provide an `id` field in the json with a value equal to the fully qualified NSID of the lexicon, with no fragments.",
+            );
+            continue;
+        }
         const nsid = relativePath.split(".")[0].split("/").join(".");
         if (content.id !== nsid) {
             console.warn(
                 "Skipping lexicon with incorrect ID",
                 `\`${content.id as string}\``,
                 "found at",
-                `\`${relativePath}\``,
-                ". Please ensure that your lexicon IDs match where they're supposed to be in the /lexicons directory.",
+                `\`${relativePath}\`.`,
+                "Please ensure that your lexicon IDs match where they're supposed to be in the /lexicons directory.",
             );
             continue;
         }
 
         // check for lexicon field value 1 (as per spec)
-        if (!("lexicon" in content)) continue;
+        if (!("lexicon" in content)) {
+            console.warn(
+                "Skipping lexicon with no `lexicon` field. Found at",
+                `\`${relativePath}\`.`,
+                "Per the spec, you need to provide a `lexicon` field in the json with a value of 1.",
+            );
+            continue;
+        }
         if (content.lexicon !== 1) {
             console.warn(
                 "Skipping lexicon with incorrect value",
                 `\`${(content.lexicon as number).toString()}\``,
                 "found at",
-                `\`${relativePath}\``,
-                ". Per the spec, please ensure that this is set to 1.",
+                `\`${relativePath}\`.`,
+                "Per the spec, please ensure that this is set to 1.",
             );
             continue;
+        }
+
+        if (!("defs" in content)) {
+            console.warn(
+                "Skipping lexicon with no `defs` field. Found at",
+                `\`${relativePath}\`.`,
+                "If the defs field is empty then you don't have a lexicon. This shouldn't happen!!!!",
+            );
         }
 
         files.push({
