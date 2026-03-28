c123ef2629397d80b2288ac6a8a7a2933558b27c
serenity <98162476+NekoDrone@users.noreply.github.com>
2026-03-28 19:24:12 +0800

    refactor: use core warning instead of console warn

--- src/read-lexicons.ts
+++ src/read-lexicons.ts
@@ -1,5 +1,6 @@
 import { readdir, readFile } from "fs/promises";
 import { extname, join, relative, resolve } from "path";
+import * as core from "@actions/core";
 
 export interface LexiconFile {
     relativePath: string;
@@ -29,7 +30,7 @@ export const readLexicons = async (workspaceRoot: string) => {
 
         // basic sanity checks
         if (typeof content !== "object" || !content) {
-            console.warn(
+            core.warning(
                 "Parsed .json file was not a properly formatted json object. Check your inputs.",
             );
             continue;
@@ -37,20 +38,20 @@ export const readLexicons = async (workspaceRoot: string) => {
 
         // check for id matching location of file
         if (!("id" in content)) {
-            console.warn(
-                "Skipping lexicon with no `id` field. Found at",
-                `\`${relativePath}\`.`,
+            core.warning(
+                "Skipping lexicon with no `id` field. Found at " +
+                `\`${relativePath}\`. ` +
                 "Per the spec, you need to provide an `id` field in the json with a value equal to the fully qualified NSID of the lexicon, with no fragments.",
             );
             continue;
         }
         const nsid = relativePath.split(".")[0].split("/").join(".");
         if (content.id !== nsid) {
-            console.warn(
-                "Skipping lexicon with incorrect ID",
-                `\`${content.id as string}\``,
-                "found at",
-                `\`${relativePath}\`.`,
+            core.warning(
+                "Skipping lexicon with incorrect ID " +
+                `\`${content.id as string}\` ` +
+                "found at " +
+                `\`${relativePath}\`. ` +
                 "Please ensure that your lexicon IDs match where they're supposed to be in the /lexicons directory.",
             );
             continue;
@@ -58,28 +59,28 @@ export const readLexicons = async (workspaceRoot: string) => {
 
         // check for lexicon field value 1 (as per spec)
         if (!("lexicon" in content)) {
-            console.warn(
-                "Skipping lexicon with no `lexicon` field. Found at",
-                `\`${relativePath}\`.`,
+            core.warning(
+                "Skipping lexicon with no `lexicon` field. Found at " +
+                `\`${relativePath}\`. ` +
                 "Per the spec, you need to provide a `lexicon` field in the json with a value of 1.",
             );
             continue;
         }
         if (content.lexicon !== 1) {
-            console.warn(
-                "Skipping lexicon with incorrect value",
-                `\`${(content.lexicon as number).toString()}\``,
-                "found at",
-                `\`${relativePath}\`.`,
+            core.warning(
+                "Skipping lexicon with incorrect value " +
+                `\`${(content.lexicon as number).toString()}\` ` +
+                "found at " +
+                `\`${relativePath}\`. ` +
                 "Per the spec, please ensure that this is set to 1.",
             );
             continue;
         }
 
         if (!("defs" in content)) {
-            console.warn(
-                "Skipping lexicon with no `defs` field. Found at",
-                `\`${relativePath}\`.`,
+            core.warning(
+                "Skipping lexicon with no `defs` field. Found at " +
+                `\`${relativePath}\`. ` +
                 "If the defs field is empty then you don't have a lexicon. This shouldn't happen!!!!",
             );
         }
